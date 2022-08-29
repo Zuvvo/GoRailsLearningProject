cff881cd356713796f2df9278afb50a280cc3f2f
Zuvvo <Zuvvo4@gmail.com>
2022-08-29 16:00:16 +0200

    sign up page

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "about-us", to: "about#index", as: :about
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registration#create"
   root to: "main#index"
 
 end
