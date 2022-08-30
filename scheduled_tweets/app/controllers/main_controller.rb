fe10bceb8355abbc3531f451c39b2fbf473acfb5
Zuvvo <Zuvvo4@gmail.com>
2022-08-30 15:28:28 +0200

    log in, log out

--- scheduled_tweets/app/controllers/main_controller.rb
+++ scheduled_tweets/app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Error"
+    #flash[:notice] = "Logged in successfully"
+    #flash[:alert] = "Error"
   end
 end
