05dad66a2f199cb127d82ebf076bbcb29dab3a32
Zuvvo <Zuvvo4@gmail.com>
2022-09-02 19:54:44 +0200

    twitter connecting, postgres db

--- scheduled_tweets/db/schema.rb
+++ scheduled_tweets/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_103205) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_105143) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "username"
+    t.string "image"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
@@ -18,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_103205) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "twitter_accounts", "users"
 end
