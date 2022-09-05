a175a20d08c40e37da140750c3b326c06406c39c
Zuvvo <Zuvvo4@gmail.com>
2022-09-05 16:51:03 +0200

    sidekiq

--- scheduled_tweets/app/jobs/tweet_job.rb
+++ scheduled_tweets/app/jobs/tweet_job.rb
@@ -1,7 +1,10 @@
 class TweetJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(tweet)
+    return if tweet.published?
+    return if tweet.publish_at > Time.current
+
+    tweet.publish_to_twitter!
   end
 end
